14fea82240d322e97b90a40c29a1ebb01c3ccd50
Oluwatobi Ogunwuyi <togunwuyi@digicoreltd.com>
2023-06-27 11:15:50 +0000

    Update BillentBackofficeServiceApplicationTests.java

--- src/test/java/com/digicore/billentbackofficeservice/BillentBackofficeServiceApplicationTests.java
+++ src/test/java/com/digicore/billentbackofficeservice/BillentBackofficeServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.digicore.billentbackofficeservice;
+package com.digicore.billent.backoffice.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
