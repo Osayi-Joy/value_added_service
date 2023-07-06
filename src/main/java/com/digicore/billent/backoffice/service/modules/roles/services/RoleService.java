27e076b9713c8ff9f7db02dd732b577017db8216
Ezenwa Opara <eopara@digicoreltd.com>
2023-07-06 14:14:43 +0100

    remove unnecessary logging

--- src/main/java/com/digicore/billent/backoffice/service/modules/roles/services/RoleService.java
+++ src/main/java/com/digicore/billent/backoffice/service/modules/roles/services/RoleService.java
@@ -6,7 +6,6 @@ import com.digicore.billent.data.lib.modules.common.authentication.model.Role;
 import com.digicore.billent.data.lib.modules.common.authentication.repository.RoleRepository;
 import com.digicore.request.processor.annotations.MakerChecker;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,7 +13,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RoleService {
@@ -22,7 +20,6 @@ public class RoleService {
 
     @MakerChecker(makerPermission = "view-roles", checkerPermission = "approve-view-roles")
     public Object getAllRoles(Object request, Object[] fileDTO) {
-        log.info("Request to get all roles.");
         return getAllRoles((Pageable) request);
     }
 
