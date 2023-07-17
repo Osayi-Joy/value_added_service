d90aa4ad46835bc9752b6f53fc8ed9cb6c9c829a
Oluwatobi Ogunwuyi <togunwuyi@digicoreltd.com>
2023-07-17 18:27:29 +0100

    updated service

---
+++ src/test/java/com/digicore/billent/backoffice/service/test/integration/common/TestHelper.java
@@ -0,0 +1,58 @@
+package com.digicore.billent.backoffice.service.test.integration.common;
+
+import com.digicore.api.helper.response.ApiResponseJson;
+import com.digicore.common.util.ClientUtil;
+import com.digicore.registhentication.authentication.dtos.request.LoginRequestDTO;
+import com.digicore.registhentication.authentication.dtos.response.LoginResponse;
+import com.digicore.registhentication.authentication.enums.AuthenticationType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.digicore.billent.backoffice.service.util.BackOfficeUserServiceApiUtil.AUTHENTICATION_API_V1;
+import static com.digicore.billent.data.lib.modules.common.constants.SystemConstants.MAKER_EMAIL;
+import static com.digicore.billent.data.lib.modules.common.constants.SystemConstants.SYSTEM_DEFAULT_PASSWORD;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/*
+ * @author Oluwatobi Ogunwuyi
+ * @createdOn Jul-17(Mon)-2023
+ */
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TestHelper {
+
+
+    private final MockMvc mockMvc;
+
+    public String retrieveMakerAccessToken() throws Exception {
+     LoginRequestDTO loginRequestDTO = new LoginRequestDTO();
+     loginRequestDTO.setAuthenticationType(AuthenticationType.PASSWORD);
+     loginRequestDTO.setPassword(SYSTEM_DEFAULT_PASSWORD);
+     loginRequestDTO.setEmail(MAKER_EMAIL);
+     loginRequestDTO.setUsername(MAKER_EMAIL);
+
+     MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTHENTICATION_API_V1.concat("login"))
+                     .content(ClientUtil.getGsonMapper().toJson(loginRequestDTO)).contentType(MediaType.APPLICATION_JSON))
+             .andExpect(status().isOk()).andReturn();
+     log.trace("the result {}", result.getResponse().getContentAsString());
+
+     ApiResponseJson<?> response = ClientUtil.getGsonMapper().fromJson(result.getResponse().getContentAsString(),ApiResponseJson.class);
+     assertTrue(response.isSuccess());
+     log.trace("the result again {}", response);
+     LoginResponse loginResponse = ClientUtil.getGsonMapper().fromJson(String.valueOf(response.getData()), LoginResponse.class);
+     log.trace("the result againnnnnn {}", loginResponse);
+     return loginResponse.getAccessToken();
+ }
+
+}
--- src/test/java/com/digicore/billent/backoffice/service/test/integration/registration/BackOfficeUserRegistrationTest.java
+++ src/test/java/com/digicore/billent/backoffice/service/test/integration/registration/BackOfficeUserRegistrationTest.java
@@ -1,52 +1,54 @@
-//package com.digicore.billent.backoffice.service.test.integration.registration;
-///*
-// * @author Oluwatobi Ogunwuyi
-// * @createdOn Jul-17(Mon)-2023
-// */
-//
-//import com.digicore.api.helper.response.ApiResponseJson;
-//import com.digicore.billent.data.lib.modules.common.authentication.dtos.UserRegistrationDTO;
-//import com.digicore.common.util.ClientUtil;
-//import org.json.JSONObject;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.TestPropertySource;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.MvcResult;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//
-//import static com.digicore.billent.backoffice.service.util.BackOfficeUserServiceApiUtil.ONBOARDING_API_V1;
-//import static com.digicore.billent.data.lib.modules.common.constants.SystemConstants.MAKER_ROLE_NAME;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureMockMvc
-//@TestPropertySource(
-//        locations = "classpath:application-integrationtest.properties")
-// class BackOfficeUserRegistrationTest {
-// @Autowired
-// private MockMvc mockMvc;
-//
-// @Test
-//  void getToken() throws Exception {
-//  UserRegistrationDTO userRegistrationDTO = new UserRegistrationDTO();
-//  userRegistrationDTO.setEmail("tobiogunwuyi@gmail.com");
-//  userRegistrationDTO.setPhoneNumber("07087982874");
-//  userRegistrationDTO.setFirstName("Oluwatobi");
-//  userRegistrationDTO.setLastName("Ogunwuyi");
-//  userRegistrationDTO.setAssignedRole(MAKER_ROLE_NAME);
-//  userRegistrationDTO.setUsername("tobiogunwuyi@gmail.com");
-//
-//
-//  MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(ONBOARDING_API_V1.concat("user-invitation"))
-//                  .content(String.valueOf(userRegistrationDTO)).contentType(MediaType.APPLICATION_JSON))
-//          .andExpect(status().isOk()).andReturn();
-//
-//  ApiResponseJson<?> response = ClientUtil.getGsonMapper().fromJson(result.getResponse().getContentAsString(),ApiResponseJson.class);
-//  assertTrue(response.isSuccess());
-// }
-//}
+package com.digicore.billent.backoffice.service.test.integration.registration;
+/*
+ * @author Oluwatobi Ogunwuyi
+ * @createdOn Jul-17(Mon)-2023
+ */
+
+import com.digicore.api.helper.response.ApiResponseJson;
+import com.digicore.billent.backoffice.service.test.integration.common.TestHelper;
+import com.digicore.billent.data.lib.modules.common.authentication.dtos.UserRegistrationDTO;
+import com.digicore.common.util.ClientUtil;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.digicore.billent.backoffice.service.util.BackOfficeUserServiceApiUtil.ONBOARDING_API_V1;
+import static com.digicore.billent.data.lib.modules.common.constants.SystemConstants.MAKER_ROLE_NAME;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-test.properties")
+ class BackOfficeUserRegistrationTest {
+ @Autowired
+ private MockMvc mockMvc;
+
+ @Test
+  void onboardNewUser() throws Exception {
+   TestHelper testHelper = new  TestHelper(mockMvc);
+  UserRegistrationDTO userRegistrationDTO = new UserRegistrationDTO();
+  userRegistrationDTO.setEmail("tobiogunwuyi@gmail.com");
+  userRegistrationDTO.setPhoneNumber("07087982874");
+  userRegistrationDTO.setFirstName("Oluwatobi");
+  userRegistrationDTO.setLastName("Ogunwuyi");
+  userRegistrationDTO.setAssignedRole(MAKER_ROLE_NAME);
+  userRegistrationDTO.setUsername("tobiogunwuyi@gmail.com");
+
+
+  MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(ONBOARDING_API_V1.concat("user-invitation"))
+                  .content(ClientUtil.getGsonMapper().toJson(userRegistrationDTO)).contentType(MediaType.APPLICATION_JSON).header("Authorization",testHelper.retrieveMakerAccessToken()))
+          .andExpect(status().isOk()).andReturn();
+
+  ApiResponseJson<?> response = ClientUtil.getGsonMapper().fromJson(result.getResponse().getContentAsString(),ApiResponseJson.class);
+  assertTrue(response.isSuccess());
+ }
+}
