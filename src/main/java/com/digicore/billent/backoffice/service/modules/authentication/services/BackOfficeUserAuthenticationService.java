3cbf75e27c4262eb44711cfd4a83e7bcf9bfb54c
Oluwatobi Ogunwuyi <togunwuyi@digicoreltd.com>
2023-07-18 09:30:19 +0100

    updated service

--- src/main/java/com/digicore/billent/backoffice/service/modules/authentication/services/BackOfficeUserAuthenticationService.java
+++ src/main/java/com/digicore/billent/backoffice/service/modules/authentication/services/BackOfficeUserAuthenticationService.java
@@ -1,8 +1,5 @@
 package com.digicore.billent.backoffice.service.modules.authentication.services;
 
-import com.digicore.billent.data.lib.modules.backoffice.authentication.dto.BackOfficeUserAuthProfileDTO;
-import com.digicore.billent.data.lib.modules.backoffice.authentication.model.BackOfficeUserAuthProfile;
-import com.digicore.billent.data.lib.modules.backoffice.authentication.service.BackOfficeUserAuthService;
 import com.digicore.config.properties.PropertyConfig;
 import com.digicore.notification.lib.request.NotificationRequestType;
 import com.digicore.notification.lib.request.NotificationServiceRequest;
@@ -10,11 +7,9 @@ import com.digicore.otp.service.NotificationDispatcher;
 import com.digicore.registhentication.authentication.dtos.request.LoginRequestDTO;
 import com.digicore.registhentication.authentication.dtos.response.LoginResponse;
 import com.digicore.registhentication.authentication.services.LoginService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 /*
  * @author Oluwatobi Ogunwuyi
@@ -23,19 +18,19 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class BackOfficeUserAuthenticationService {
-    private final LoginService<LoginResponse, LoginRequestDTO> userAuthService;
-    private final NotificationDispatcher notificationDispatcher;
-    private final PropertyConfig propertyConfig;
+  private final LoginService<LoginResponse, LoginRequestDTO> userAuthService;
+  private final NotificationDispatcher notificationDispatcher;
+  private final PropertyConfig propertyConfig;
 
-    public LoginResponse authenticateBackOfficeUser(LoginRequestDTO loginRequestDTO){
-        LoginResponse loginResponse = userAuthService.authenticate(loginRequestDTO);
-        notificationDispatcher.dispatchEmail(NotificationServiceRequest.builder()
-                .recipients(List.of(loginResponse.getAdditionalInformation().get("email").toString()))
-                .notificationSubject(propertyConfig.getSuccessLoginSubject())
-                .firstName((String) loginResponse.getAdditionalInformation().get("firstName"))
-                .notificationRequestType(NotificationRequestType.SEND_LOGIN_SUCCESS_EMAIL)
-                .build());
-        return loginResponse;
-    }
-
+  public LoginResponse authenticateBackOfficeUser(LoginRequestDTO loginRequestDTO) {
+    LoginResponse loginResponse = userAuthService.authenticate(loginRequestDTO);
+    notificationDispatcher.dispatchEmail(
+        NotificationServiceRequest.builder()
+            .recipients(List.of(loginResponse.getAdditionalInformation().get("email").toString()))
+            .notificationSubject(propertyConfig.getSuccessLoginSubject())
+            .firstName((String) loginResponse.getAdditionalInformation().get("firstName"))
+            .notificationRequestType(NotificationRequestType.SEND_LOGIN_SUCCESS_EMAIL)
+            .build());
+    return loginResponse;
+  }
 }
--- src/main/java/com/digicore/billent/backoffice/service/util/SwaggerDocUtil.java
+++ src/main/java/com/digicore/billent/backoffice/service/util/SwaggerDocUtil.java
@@ -6,9 +6,12 @@ package com.digicore.billent.backoffice.service.util;
 
 public class SwaggerDocUtil {
 
+ private SwaggerDocUtil() {
+ }
+
  //Approval Controller Doc
- public static final String APPROVAL_CONTROLLER_TITLE = "BackOffice-Approval-Controller";
- public static final String APPROVAL_CONTROLLER_DESCRIPTION = "This controller contains all required APIs to complete the checker operations in the system.";
+ public static final String APPROVAL_CONTROLLER_TITLE = "BackOffice-Approval-Module";
+ public static final String APPROVAL_CONTROLLER_DESCRIPTION = "This module contains all required APIs to complete the checker operations in the system.";
 
  public static final String APPROVAL_CONTROLLER_APPROVE_REQUEST_TITLE = "Approve pending request";
  public static final String APPROVAL_CONTROLLER_APPROVE_REQUEST_DESCRIPTION = "This API is used to approve a pending request, it requires the authenticated user has the right approve permission based on the pending request type";
@@ -23,6 +26,15 @@ public class SwaggerDocUtil {
 
  public static final String APPROVAL_CONTROLLER_GET_PENDING_REQUEST_TITLE = "Get all pending request";
  public static final String APPROVAL_CONTROLLER_GET_PENDING_REQUEST_DESCRIPTION = "This API is used to get all pending request.";
+
+
+
+ //Authentication Controller Doc
+ public static final String AUTHENTICATION_CONTROLLER_TITLE = "BackOffice-Authentication-Module";
+ public static final String AUTHENTICATION_CONTROLLER_DESCRIPTION = "This module contains all required APIs to complete authentication.";
+
+ public static final String AUTHENTICATION_CONTROLLER_LOGIN_TITLE = "Authenticate a user";
+ public static final String AUTHENTICATION_CONTROLLER_LOGIN_DESCRIPTION = "This API is used to authenticate a user.";
 
 }
 
