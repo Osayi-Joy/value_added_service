a52f3b5fd8d5fe1cee0aafdd118e41121934b325
Oluwatobi Ogunwuyi <togunwuyi@digicoreltd.com>
2023-07-18 10:01:57 +0100

    updated service

--- src/main/java/com/digicore/billent/backoffice/service/modules/roles/controller/RoleController.java
+++ src/main/java/com/digicore/billent/backoffice/service/modules/roles/controller/RoleController.java
@@ -1,7 +1,13 @@
 package com.digicore.billent.backoffice.service.modules.roles.controller;
 
+import static com.digicore.billent.backoffice.service.util.BackOfficeUserServiceApiUtil.ROLES_API_V1;
+import static com.digicore.billent.backoffice.service.util.SwaggerDocUtil.*;
+import static com.digicore.billent.data.lib.modules.common.util.BackOfficePageableUtil.*;
+
 import com.digicore.api.helper.response.ControllerResponse;
 import com.digicore.billent.backoffice.service.modules.roles.services.BackOfficeRoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,19 +16,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.digicore.billent.backoffice.service.util.BackOfficeUserServiceApiUtil.ROLES_API_V1;
-import static com.digicore.billent.data.lib.modules.common.util.BackOfficePageableUtil.*;
-
 @RestController
 @RequestMapping(ROLES_API_V1)
 @RequiredArgsConstructor
+@Tag(name = ROLE_CONTROLLER_TITLE, description = ROLE_CONTROLLER_DESCRIPTION)
 public class RoleController {
-    private final BackOfficeRoleService backOfficeRoleService;
+  private final BackOfficeRoleService backOfficeRoleService;
 
-    @GetMapping("get-all-roles")
-    @PreAuthorize("hasAuthority('view-roles')")
-    public ResponseEntity<Object> getAllRoles(@RequestParam(value = PAGE_NUMBER, defaultValue = PAGE_NUMBER_DEFAULT_VALUE, required = false) int pageNumber,
-                                              @RequestParam(value = PAGE_SIZE, defaultValue = PAGE_SIZE_DEFAULT_VALUE, required = false) int pageSize) {
-        return ControllerResponse.buildSuccessResponse(backOfficeRoleService.getAllRoles(pageNumber, pageSize), "Roles retrieved successfully");
-    }
+  @GetMapping("get-all-roles")
+  @PreAuthorize("hasAuthority('view-roles')")
+  @Operation(
+      summary = ROLE_CONTROLLER_GET_ALL_ROLES_TITLE,
+      description = ROLE_CONTROLLER_GET_ALL_ROLES_DESCRIPTION)
+  public ResponseEntity<Object> getAllRoles(
+      @RequestParam(value = PAGE_NUMBER, defaultValue = PAGE_NUMBER_DEFAULT_VALUE, required = false)
+          int pageNumber,
+      @RequestParam(value = PAGE_SIZE, defaultValue = PAGE_SIZE_DEFAULT_VALUE, required = false)
+          int pageSize) {
+    return ControllerResponse.buildSuccessResponse(
+        backOfficeRoleService.getAllRoles(pageNumber, pageSize), "Roles retrieved successfully");
+  }
 }
--- src/main/java/com/digicore/billent/backoffice/service/util/SwaggerDocUtil.java
+++ src/main/java/com/digicore/billent/backoffice/service/util/SwaggerDocUtil.java
@@ -6,45 +6,38 @@ package com.digicore.billent.backoffice.service.util;
 
 public class SwaggerDocUtil {
 
- private SwaggerDocUtil() {
- }
-
  //Approval Controller Doc
  public static final String APPROVAL_CONTROLLER_TITLE = "BackOffice-Approval-Module";
  public static final String APPROVAL_CONTROLLER_DESCRIPTION = "This module contains all required APIs to complete the checker operations in the system.";
-
  public static final String APPROVAL_CONTROLLER_APPROVE_REQUEST_TITLE = "Approve pending request";
  public static final String APPROVAL_CONTROLLER_APPROVE_REQUEST_DESCRIPTION = "This API is used to approve a pending request, it requires the authenticated user has the right approve permission based on the pending request type";
  public static final String APPROVAL_CONTROLLER_DECLINE_REQUEST_TITLE = "Decline pending request";
  public static final String APPROVAL_CONTROLLER_DECLINE_REQUEST_DESCRIPTION = "This API is used to decline a pending request.";
-
  public static final String APPROVAL_CONTROLLER_GET_REQUEST_TITLE = "Get a single pending request";
  public static final String APPROVAL_CONTROLLER_GET_REQUEST_DESCRIPTION = "This API is used to get a pending request.";
-
  public static final String APPROVAL_CONTROLLER_GET_TREATED_REQUEST_TITLE = "Get treated request";
  public static final String APPROVAL_CONTROLLER_GET_TREATED_REQUEST_DESCRIPTION = "This API is used to get all treated request.";
-
  public static final String APPROVAL_CONTROLLER_GET_PENDING_REQUEST_TITLE = "Get all pending request";
  public static final String APPROVAL_CONTROLLER_GET_PENDING_REQUEST_DESCRIPTION = "This API is used to get all pending request.";
-
-
-
  //Authentication Controller Doc
  public static final String AUTHENTICATION_CONTROLLER_TITLE = "BackOffice-Authentication-Module";
  public static final String AUTHENTICATION_CONTROLLER_DESCRIPTION = "This module contains all required APIs to complete authentication.";
-
  public static final String AUTHENTICATION_CONTROLLER_LOGIN_TITLE = "Authenticate a user";
  public static final String AUTHENTICATION_CONTROLLER_LOGIN_DESCRIPTION = "This API is used to authenticate a user.";
-
  //Onboarding Controller Doc
  public static final String ONBOARDING_CONTROLLER_TITLE = "BackOffice-Onboarding-Module";
  public static final String ONBOARDING_CONTROLLER_DESCRIPTION = "This module contains all required APIs to invite new users into the system.";
-
  public static final String ONBOARDING_CONTROLLER_INVITE_USER_TITLE = "Invite a user to the backoffice platform, this module goes through maker checker process.";
  public static final String ONBOARDING_CONTROLLER_INVITE_USER_DESCRIPTION = "This API is used to invite a user to the backoffice platform, it sends an invite mail to the user.";
-
  public static final String ONBOARDING_CONTROLLER_RE_INVITE_USER_TITLE = "Resend invitation to an already invited user";
  public static final String ONBOARDING_CONTROLLER_RE_INVITE_USER_DESCRIPTION = "This API is used to resend invitation to an already invited user.This is useful for when they didn't get the previous invite mail";
+ //Role Controller Doc
+ public static final String ROLE_CONTROLLER_TITLE = "BackOffice-Role-Module";
+ public static final String ROLE_CONTROLLER_DESCRIPTION = "This module contains all required APIs to complete operations surrounding authorization ( role/permission management ). it goes through maker checker process";
+ public static final String ROLE_CONTROLLER_GET_ALL_ROLES_TITLE = "Authenticate a user";
+ public static final String ROLE_CONTROLLER_GET_ALL_ROLES_DESCRIPTION = "This API is used to authenticate a user.";
+ private SwaggerDocUtil() {
+ }
 
 }
 
