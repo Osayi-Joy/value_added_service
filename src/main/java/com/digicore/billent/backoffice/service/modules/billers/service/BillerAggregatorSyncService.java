714bcce443dece2a740d7d1f6d546b2fa96014fe
Oluwatobi Ogunwuyi <togunwuyi@digicoreltd.com>
2023-07-27 05:50:51 +0100

    updated MR

---
+++ src/main/java/com/digicore/billent/backoffice/service/modules/billers/service/BillerAggregatorSyncService.java
@@ -0,0 +1,26 @@
+package com.digicore.billent.backoffice.service.modules.billers.service;
+
+import com.digicore.billent.data.lib.modules.billers.aggregator.dto.BillerAggregatorDTO;
+import com.digicore.billent.data.lib.modules.billers.aggregator.service.BillerAggregatorService;
+import com.digicore.request.processor.annotations.RequestHandler;
+import com.digicore.request.processor.annotations.RequestType;
+import com.digicore.request.processor.enums.RequestHandlerType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+/*
+ * @author Oluwatobi Ogunwuyi
+ * @createdOn Jul-27(Thu)-2023
+ */
+@RequestHandler(type = RequestHandlerType.PROCESS_MAKER_REQUESTS)
+@RequiredArgsConstructor
+public class BillerAggregatorSyncService {
+
+    @Qualifier("EtranzactServiceImpl")
+    private final BillerAggregatorService billerAggregatorService;
+
+    @RequestType(name = "ETRANZACT")
+    public void process(BillerAggregatorDTO request)  {
+        billerAggregatorService.refreshAggregatorBillersAndProducts(request.getAggregatorAlias());
+    }
+}
