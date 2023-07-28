27c713518ca5cee6a8a61347e7e09ab0d636f459
Joy Osayi <osayijoy16@gmail.com>
2023-07-28 08:28:16 +0100

    Enable a Biller #13 updates to perform a check for biller

---
+++ src/main/java/com/digicore/billent/backoffice/service/modules/billers/service/BillerBackOfficeValidatorService.java
@@ -0,0 +1,5 @@
+package com.digicore.billent.backoffice.service.modules.billers.service;
+
+public interface BillerBackOfficeValidatorService {
+    Object enableBiller(Object request, Object... args);
+}
