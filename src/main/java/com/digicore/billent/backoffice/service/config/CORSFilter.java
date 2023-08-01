53a17fc0530d6f55928e3f654885c4d46de2434e
Oluwatobi Ogunwuyi <togunwuyi@digicoreltd.com>
2023-08-01 16:33:03 +0100

    CORS fix

--- src/main/java/com/digicore/billent/backoffice/service/config/CORSFilter.java
+++ src/main/java/com/digicore/billent/backoffice/service/config/CORSFilter.java
@@ -1,28 +1,29 @@
-//package com.digicore.billent.backoffice.service.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-///**
-// * @author Oluwatobi Ogunwuyi
-// * @createdOn Sep-05(Mon)-2022
-// */
-//@Configuration
-//public class CORSFilter implements WebMvcConfigurer {
-//  @Override
-//  public void addCorsMappings(CorsRegistry registry) {
-//    registry
-//        .addMapping("/**")
-//        .allowedOrigins("*")
-//        .allowedHeaders(
-//            "Content-Type",
-//            "Access-Control-Allow-Headers",
-//            "Access-Control-Expose-Headers",
-//            "Content-Disposition",
-//            "Authorization",
-//            " X-Requested-With")
-//        .exposedHeaders("Content-Disposition")
-//        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH");
-//  }
-//}
+package com.digicore.billent.backoffice.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Oluwatobi Ogunwuyi
+ * @createdOn Sep-05(Mon)-2022
+ */
+@Configuration
+public class CORSFilter implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins("*")
+        .allowedHeaders(
+            "Content-Type",
+            "Access-Control-Allow-Headers",
+            "Access-Control-Expose-Headers",
+            "Content-Disposition",
+                "Access-Control-Allow-Origin",
+            "Authorization",
+            " X-Requested-With")
+        .exposedHeaders("Content-Disposition")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH");
+  }
+}
