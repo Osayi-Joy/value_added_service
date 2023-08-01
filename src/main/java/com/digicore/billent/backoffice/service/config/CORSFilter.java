02f323989b2e1881ddbc3f0f08b3eb4f54df1d5c
Oluwatobi Ogunwuyi <togunwuyi@digicoreltd.com>
2023-08-01 18:05:10 +0100

    CORS fix-v4

--- src/main/java/com/digicore/billent/backoffice/service/config/CORSFilter.java
+++ src/main/java/com/digicore/billent/backoffice/service/config/CORSFilter.java
@@ -1,14 +1,10 @@
 package com.digicore.billent.backoffice.service.config;
 
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 /**
  * @author Oluwatobi Ogunwuyi
@@ -17,19 +13,21 @@ import java.util.List;
 @Configuration
 public class CORSFilter implements CorsConfigurationSource {
 
-    @Override
-    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"));
-        config.setAllowedHeaders(List.of( "Content-Type",
-                "Access-Control-Allow-Headers",
-                "Access-Control-Expose-Headers",
-//                      "Access-Control-Allow-Origin",
-                "Content-Disposition",
-                "Authorization",
-                " X-Requested-With"));
-        config.addExposedHeader("Content-Disposition");
-        return config;
-    }
+  @Override
+  public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("*"));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"));
+    config.setAllowedHeaders(
+        List.of(
+            "Content-Type",
+            "Access-Control-Allow-Headers",
+            "Access-Control-Expose-Headers",
+            "Access-Control-Allow-Origin",
+            "Content-Disposition",
+            "Authorization",
+            " X-Requested-With"));
+    config.addExposedHeader("Content-Disposition");
+    return config;
+  }
 }
