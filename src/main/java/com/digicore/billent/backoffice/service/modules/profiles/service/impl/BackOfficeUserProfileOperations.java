5c1c47af8c6b8fc12e378ae92ff370c5b01d2ff5
Joy Osayi <osayijoy16@gmail.com>
2023-08-16 13:06:54 +0100

    made the updates based on the comments on the MR

--- src/main/java/com/digicore/billent/backoffice/service/modules/profiles/service/impl/BackOfficeUserProfileOperations.java
+++ src/main/java/com/digicore/billent/backoffice/service/modules/profiles/service/impl/BackOfficeUserProfileOperations.java
@@ -1,6 +1,7 @@
 package com.digicore.billent.backoffice.service.modules.profiles.service.impl;
 
 import com.digicore.billent.backoffice.service.modules.profiles.service.BackOfficeUserProfileValidatorService;
+import com.digicore.billent.data.lib.modules.backoffice.profile.model.BackOfficeUserProfile;
 import com.digicore.billent.data.lib.modules.common.authentication.dto.UserProfileDTO;
 import com.digicore.billent.data.lib.modules.common.profile.UserProfileService;
 import com.digicore.billent.data.lib.modules.common.util.BillentSearchRequest;
@@ -17,7 +18,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class BackOfficeUserProfileOperations implements BackOfficeUserProfileValidatorService {
-  private final UserProfileService<UserProfileDTO> backOfficeUserProfileServiceImpl;
+  private final UserProfileService<UserProfileDTO, BackOfficeUserProfile> backOfficeUserProfileServiceImpl;
 
   public PaginatedResponseDTO<UserProfileDTO> fetchAllBackOfficeUserProfiles(int page, int size) {
     return backOfficeUserProfileServiceImpl.retrieveAllUserProfiles(page, size);
