94aa21834fb8e8781591ae477cef58027199f176
Oluwatobi Ogunwuyi <togunwuyi@digicoreltd.com>
2023-09-18 11:46:12 +0100

    pushing changes

--- src/main/java/com/digicore/billent/backoffice/service/modules/billers/processor/BackOfficeBillerProcessor.java
+++ src/main/java/com/digicore/billent/backoffice/service/modules/billers/processor/BackOfficeBillerProcessor.java
@@ -5,6 +5,7 @@ import com.digicore.request.processor.annotations.RequestHandler;
 import com.digicore.request.processor.annotations.RequestType;
 import com.digicore.request.processor.enums.RequestHandlerType;
 import lombok.RequiredArgsConstructor;
+
 /*
  * @author Joy Osayi
  * @createdOn Jul-03(Mon)-2023
@@ -12,29 +13,20 @@ import lombok.RequiredArgsConstructor;
 @RequestHandler(type = RequestHandlerType.PROCESS_MAKER_REQUESTS)
 @RequiredArgsConstructor
 public class BackOfficeBillerProcessor {
-    private final BillerBackOfficeService billerBackOfficeService;
-    private final ProductBackOfficeService productBackOfficeService;
-    @RequestType(name = "updateBillerDetail")
-    public Object updateBillerDetail(Object request){
-        return billerBackOfficeService.updateBillerDetail(request);
-    }
+  private final BillerBackOfficeService billerBackOfficeService;
 
-    @RequestType(name = "enableBiller")
-    public Object enableBiller(Object request){
-        return billerBackOfficeService.enableBiller(request);
-    }
-    @RequestType(name = "disableBiller")
-    public Object disableBiller(Object request){
-        return billerBackOfficeService.disableBiller(request);
-    }
-    @RequestType(name = "enableProduct")
-    public Object enableProduct(Object request){
-        return productBackOfficeService.enableProduct(request);
-    }
+  @RequestType(name = "updateBillerDetail")
+  public Object updateBillerDetail(Object request) {
+    return billerBackOfficeService.updateBillerDetail(request);
+  }
 
-    @RequestType(name = "disableProduct")
-    public Object disableProduct(Object request){
-        return productBackOfficeService.disableProduct(request);
-    }
+  @RequestType(name = "enableBiller")
+  public Object enableBiller(Object request) {
+    return billerBackOfficeService.enableBiller(request);
+  }
 
+  @RequestType(name = "disableBiller")
+  public Object disableBiller(Object request) {
+    return billerBackOfficeService.disableBiller(request);
+  }
 }
