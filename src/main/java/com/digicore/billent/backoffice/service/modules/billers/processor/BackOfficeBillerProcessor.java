9a2ce86b4c07dadc64beccc5bdff59ee498f17ae
Oluwatobi Ogunwuyi <togunwuyi@digicoreltd.com>
2023-09-18 11:52:48 +0100

    pushing changes

--- src/main/java/com/digicore/billent/backoffice/service/modules/billers/processor/BackOfficeBillerProcessor.java
+++ src/main/java/com/digicore/billent/backoffice/service/modules/billers/processor/BackOfficeBillerProcessor.java
@@ -1,32 +1,32 @@
-package com.digicore.billent.backoffice.service.modules.billers.processor;
-
-import com.digicore.billent.backoffice.service.modules.billers.service.BillerBackOfficeService;
-import com.digicore.request.processor.annotations.RequestHandler;
-import com.digicore.request.processor.annotations.RequestType;
-import com.digicore.request.processor.enums.RequestHandlerType;
-import lombok.RequiredArgsConstructor;
-
-/*
- * @author Joy Osayi
- * @createdOn Jul-03(Mon)-2023
- */
-@RequestHandler(type = RequestHandlerType.PROCESS_MAKER_REQUESTS)
-@RequiredArgsConstructor
-public class BackOfficeBillerProcessor {
-  private final BillerBackOfficeService billerBackOfficeService;
-
-  @RequestType(name = "updateBillerDetail")
-  public Object updateBillerDetail(Object request) {
-    return billerBackOfficeService.updateBillerDetail(request);
-  }
-
-  @RequestType(name = "enableBiller")
-  public Object enableBiller(Object request) {
-    return billerBackOfficeService.enableBiller(request);
-  }
-
-  @RequestType(name = "disableBiller")
-  public Object disableBiller(Object request) {
-    return billerBackOfficeService.disableBiller(request);
-  }
-}
+//package com.digicore.billent.backoffice.service.modules.billers.processor;
+//
+//import com.digicore.billent.backoffice.service.modules.billers.service.BillerBackOfficeService;
+//import com.digicore.request.processor.annotations.RequestHandler;
+//import com.digicore.request.processor.annotations.RequestType;
+//import com.digicore.request.processor.enums.RequestHandlerType;
+//import lombok.RequiredArgsConstructor;
+//
+///*
+// * @author Joy Osayi
+// * @createdOn Jul-03(Mon)-2023
+// */
+//@RequestHandler(type = RequestHandlerType.PROCESS_MAKER_REQUESTS)
+//@RequiredArgsConstructor
+//public class BackOfficeBillerProcessor {
+//  private final BillerBackOfficeService billerBackOfficeService;
+//
+//  @RequestType(name = "updateBillerDetail")
+//  public Object updateBillerDetail(Object request) {
+//    return billerBackOfficeService.updateBillerDetail(request);
+//  }
+//
+//  @RequestType(name = "enableBiller")
+//  public Object enableBiller(Object request) {
+//    return billerBackOfficeService.enableBiller(request);
+//  }
+//
+//  @RequestType(name = "disableBiller")
+//  public Object disableBiller(Object request) {
+//    return billerBackOfficeService.disableBiller(request);
+//  }
+//}
--- src/main/java/com/digicore/billent/backoffice/service/modules/billers/service/BillerBackOfficeService.java
+++ src/main/java/com/digicore/billent/backoffice/service/modules/billers/service/BillerBackOfficeService.java
@@ -1,87 +1,87 @@
-package com.digicore.billent.backoffice.service.modules.billers.service;
-
-import com.digicore.billent.backoffice.service.modules.billers.service.BillerBackOfficeValidatorService;
-import com.digicore.billent.data.lib.modules.billers.dto.BillerDto;
-import com.digicore.billent.data.lib.modules.billers.model.Biller;
-import com.digicore.billent.data.lib.modules.billers.service.BillerService;
-import com.digicore.billent.data.lib.modules.common.dto.CsvDto;
-import com.digicore.billent.data.lib.modules.common.services.CsvService;
-import com.digicore.billent.data.lib.modules.common.util.BillentSearchRequest;
-import com.digicore.registhentication.common.dto.response.PaginatedResponseDTO;
-import com.digicore.registhentication.registration.enums.Status;
-import com.digicore.request.processor.annotations.MakerChecker;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-/*
- * @author Joy Osayi
- * @createdOn Jul-03(Mon)-2023
- */
-@Service
-@RequiredArgsConstructor
-public class BillerBackOfficeService implements BillerBackOfficeValidatorService {
-  private final BillerService<BillerDto, Biller> billerService;
-  private final CsvService csvService;
-
-  public PaginatedResponseDTO<BillerDto> getAllBillers(int pageNumber, int pageSize) {
-    return billerService.retrieveAllBillers(pageNumber, pageSize);
-  }
-
-  public PaginatedResponseDTO<BillerDto> fetchBillersByStatus(
-      Status billerStatus, String startDate, String endDate, int pageNumber, int pageSize) {
-    return billerService.filterBillersByStatus(
-        billerStatus, startDate, endDate, pageNumber, pageSize);
-  }
-
-  public void downloadAllBillersInCSV(
-      HttpServletResponse response,
-      Status billerStatus,
-      String startDate,
-      String endDate,
-      String downLoadFormat,
-      int pageNumber,
-      int pageSize) {
-    BillentSearchRequest searchRequest = new BillentSearchRequest();
-    searchRequest.setStatus(billerStatus);
-    searchRequest.setStartDate(startDate);
-    searchRequest.setEndDate(endDate);
-    searchRequest.setDownloadFormat(downLoadFormat);
-
-    CsvDto<BillerDto> csvDto = new CsvDto<>();
-    csvDto.setBillentSearchRequest(searchRequest);
-    csvDto.setResponse(response);
-    csvDto.setPage(pageNumber);
-    csvDto.setPageSize(pageSize);
-    csvService.prepareCSVExport(csvDto, billerService::prepareCSV);
-  }
-
-  public BillerDto fetchBillerById(String billerSystemId) {
-    return billerService.retrieveBillerDetailsById(billerSystemId);
-  }
-
-
-  @MakerChecker(
-          checkerPermission = "approve-enable-biller",
-          makerPermission = "enable-biller",
-          requestClassName = "com.digicore.billent.data.lib.modules.billers.dto.BillerDto")
-  public Object enableBiller(Object request, Object... args) {
-    BillerDto billerDto = (BillerDto) request;
-    return billerService.enableBiller(billerDto.getBillerSystemId());
-  }
-  @MakerChecker(
-          checkerPermission = "approve-disable-biller",
-          makerPermission = "disable-biller",
-          requestClassName = "com.digicore.billent.data.lib.modules.billers.dto.BillerDto")
-  public Object disableBiller(Object request, Object... args) {
-    BillerDto billerDto = (BillerDto) request;
-    return billerService.disableBiller(billerDto.getBillerSystemId());
-  }
-
-  @MakerChecker(
-          checkerPermission = "approve-edit-billers",
-          makerPermission = "edit-billers",
-          requestClassName = "com.digicore.billent.data.lib.modules.billers.dto.BillerDto")
-  public Object updateBillerDetail(Object request, Object... args) {
-    return billerService.editBiller((BillerDto) request);
-  }
-}
+//package com.digicore.billent.backoffice.service.modules.billers.service;
+//
+//import com.digicore.billent.backoffice.service.modules.billers.service.BillerBackOfficeValidatorService;
+//import com.digicore.billent.data.lib.modules.billers.dto.BillerDto;
+//import com.digicore.billent.data.lib.modules.billers.model.Biller;
+//import com.digicore.billent.data.lib.modules.billers.service.BillerService;
+//import com.digicore.billent.data.lib.modules.common.dto.CsvDto;
+//import com.digicore.billent.data.lib.modules.common.services.CsvService;
+//import com.digicore.billent.data.lib.modules.common.util.BillentSearchRequest;
+//import com.digicore.registhentication.common.dto.response.PaginatedResponseDTO;
+//import com.digicore.registhentication.registration.enums.Status;
+//import com.digicore.request.processor.annotations.MakerChecker;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+///*
+// * @author Joy Osayi
+// * @createdOn Jul-03(Mon)-2023
+// */
+//@Service
+//@RequiredArgsConstructor
+//public class BillerBackOfficeService implements BillerBackOfficeValidatorService {
+//  private final BillerService<BillerDto, Biller> billerService;
+//  private final CsvService csvService;
+//
+//  public PaginatedResponseDTO<BillerDto> getAllBillers(int pageNumber, int pageSize) {
+//    return billerService.retrieveAllBillers(pageNumber, pageSize);
+//  }
+//
+//  public PaginatedResponseDTO<BillerDto> fetchBillersByStatus(
+//      Status billerStatus, String startDate, String endDate, int pageNumber, int pageSize) {
+//    return billerService.filterBillersByStatus(
+//        billerStatus, startDate, endDate, pageNumber, pageSize);
+//  }
+//
+//  public void downloadAllBillersInCSV(
+//      HttpServletResponse response,
+//      Status billerStatus,
+//      String startDate,
+//      String endDate,
+//      String downLoadFormat,
+//      int pageNumber,
+//      int pageSize) {
+//    BillentSearchRequest searchRequest = new BillentSearchRequest();
+//    searchRequest.setStatus(billerStatus);
+//    searchRequest.setStartDate(startDate);
+//    searchRequest.setEndDate(endDate);
+//    searchRequest.setDownloadFormat(downLoadFormat);
+//
+//    CsvDto<BillerDto> csvDto = new CsvDto<>();
+//    csvDto.setBillentSearchRequest(searchRequest);
+//    csvDto.setResponse(response);
+//    csvDto.setPage(pageNumber);
+//    csvDto.setPageSize(pageSize);
+//    csvService.prepareCSVExport(csvDto, billerService::prepareCSV);
+//  }
+//
+//  public BillerDto fetchBillerById(String billerSystemId) {
+//    return billerService.retrieveBillerDetailsById(billerSystemId);
+//  }
+//
+//
+//  @MakerChecker(
+//          checkerPermission = "approve-enable-biller",
+//          makerPermission = "enable-biller",
+//          requestClassName = "com.digicore.billent.data.lib.modules.billers.dto.BillerDto")
+//  public Object enableBiller(Object request, Object... args) {
+//    BillerDto billerDto = (BillerDto) request;
+//    return billerService.enableBiller(billerDto.getBillerSystemId());
+//  }
+//  @MakerChecker(
+//          checkerPermission = "approve-disable-biller",
+//          makerPermission = "disable-biller",
+//          requestClassName = "com.digicore.billent.data.lib.modules.billers.dto.BillerDto")
+//  public Object disableBiller(Object request, Object... args) {
+//    BillerDto billerDto = (BillerDto) request;
+//    return billerService.disableBiller(billerDto.getBillerSystemId());
+//  }
+//
+//  @MakerChecker(
+//          checkerPermission = "approve-edit-billers",
+//          makerPermission = "edit-billers",
+//          requestClassName = "com.digicore.billent.data.lib.modules.billers.dto.BillerDto")
+//  public Object updateBillerDetail(Object request, Object... args) {
+//    return billerService.editBiller((BillerDto) request);
+//  }
+//}
