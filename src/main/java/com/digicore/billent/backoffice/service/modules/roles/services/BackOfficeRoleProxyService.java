f5762f56485008469f92efff9baf37ae911dc402
Ademiju <tademiju@digicoreltd.com>
2023-10-23 12:49:40 +0100

    invalid role error fix

--- src/main/java/com/digicore/billent/backoffice/service/modules/roles/services/BackOfficeRoleProxyService.java
+++ src/main/java/com/digicore/billent/backoffice/service/modules/roles/services/BackOfficeRoleProxyService.java
@@ -30,11 +30,10 @@ public class BackOfficeRoleProxyService {
  private final ExceptionHandler<String, String, HttpStatus, String> exceptionHandler;
 
  public Object createNewRole(RoleCreationDTO roleDTO) {
-  if (null == roleDTO.getPermissions() || roleDTO.getPermissions().isEmpty())
-        exceptionHandler.processBadRequestException(PERMISSIONS_REQUIRED_MESSAGE,PERMISSIONS_REQUIRED_CODE,PERMISSIONS_REQUIRED_CODE);
+  backOfficeRoleServiceImpl.permissionCheck(roleDTO);
+  backOfficeRoleServiceImpl.checkRoleIfExist(roleDTO.getName());
   backOfficeRoleServiceImpl.checkIfRoleIsNotSystemRole(roleDTO.getName());
   backOfficePermissionServiceImpl.getValidPermissions(roleDTO.getPermissions());
-  backOfficeRoleServiceImpl.roleCheck(roleDTO.getName());
   return validatorService.createNewRole(roleDTO);
  }
 
