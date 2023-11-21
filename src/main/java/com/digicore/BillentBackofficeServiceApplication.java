366f22e2a2606263be516dcbd5a65570ed5ee5b9
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2023-11-21 11:10:13 +0100

    Optimized get-all & get-details end-points by caching with redis

--- src/main/java/com/digicore/BillentBackofficeServiceApplication.java
+++ src/main/java/com/digicore/BillentBackofficeServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 //scanBasePackages = {"com.digicore","com.digicore.billent.data.lib","com.digicore.billent.backoffice.service","com.digicore.registhentication"},
 @EnableAsync
+@EnableCaching
 @SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class BillentBackofficeServiceApplication {
 
